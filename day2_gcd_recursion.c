d5d0a46e11c0512950c5e0b2bd54593475d5197a
keertanahunshyal <keertanahunshyal2006@gmail.com>
2026-05-01 12:52:41 +0530

    Day 2: GCD using recursion

---
+++ day2_gcd_recursion.c
@@ -0,0 +1,26 @@
+// Day 2
+// Program: Find GCD using recursion
+// Concept: Recursion and Euclidean algorithm
+
+#include <stdio.h>
+
+// Recursive function to find GCD
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    int result = gcd(a, b);
+
+    printf("GCD of %d and %d is %d\n", a, b, result);
+
+    return 0;
+}
