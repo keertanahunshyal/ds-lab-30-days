a128f5d553704b25eab7c365a9ef85c720974986
keertanahunshyal <keertanahunshyal2006@gmail.com>
2026-05-05 23:05:28 +0530

    Day 5: Tower of Hanoi using recursion

---
+++ day5_tower_of_hanoi.c
@@ -0,0 +1,29 @@
+// Day 5
+// Program: Tower of Hanoi
+// Concept: Recursion (move n-1 disks, move nth disk, move n-1 again)
+
+#include <stdio.h>
+
+// Recursive function
+void hanoi(int n, char from, char to, char aux) {
+    if (n == 1) {
+        printf("Move disk 1 from %c to %c\n", from, to);
+        return;
+    }
+
+    hanoi(n - 1, from, aux, to);  // move n-1 disks to auxiliary
+    printf("Move disk %d from %c to %c\n", n, from, to); // move nth disk
+    hanoi(n - 1, aux, to, from);  // move back to destination
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of disks: ");
+    scanf("%d", &n);
+
+    printf("Steps to solve Tower of Hanoi:\n");
+    hanoi(n, 'A', 'C', 'B');  // A=source, C=destination, B=auxiliary
+
+    return 0;
+}
